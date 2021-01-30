3f367dfc124ca8d57bfa0ba925bb7b792574b97b
Frank Santos <frankmms@gmail.com>
2021-01-30 11:22:35 -0300

    Create Exercicio2.kt
    Implementação do exercicio 2.

---
+++ src/main/kotlin/dev/estudos/kotlin/exercicios/Exercicio2.kt
@@ -0,0 +1,50 @@
+package dev.estudos.kotlin.exercicios
+
+import kotlin.math.pow
+
+/**
+Escreva um algoritmo que leia três números inteiros e positivos (A, B, C) e calcule a seguinte expressão:
+  D = (R + S) / 2
+onde:
+  R = (A + B)²
+  S = (B + C)²
+*/
+
+fun main() {
+    println("""
+    Informe os valores A, B e C para calcular a formula abaixo:
+      D = (R + S) / 2
+    onde:
+      R = (A + B)²    e    S = (B + C)² 
+    """.trimIndent())
+
+    val a = readInt("Informe o valor de A: ")
+    val b = readInt("Informe o valor de B: ")
+    val c = readInt("Informe o valor de C: ")
+
+    val r = calcularR(a, b)
+    val s = calcularS(b, c)
+
+    val d = calcularD(r, s)
+
+    println("""
+        
+        -----------------------------
+        R = $r | S = $s
+        D = $d
+        -----------------------------
+        
+    """.trimIndent())
+}
+
+fun calcularD(r: Int, s: Int): Int {
+    return (r + s) / 2
+}
+
+fun calcularR(a: Int, b: Int): Int {
+    return (a + b).toDouble().pow(2).toInt()
+}
+
+fun calcularS(b: Int, c: Int): Int {
+    return (b + c).toDouble().pow(2).toInt()
+}
